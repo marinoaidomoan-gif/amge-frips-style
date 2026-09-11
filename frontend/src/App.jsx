eecb714eb5347e4a802648e6b8eba1c93e45196a
marinoaidomoan <marinoaidomoan@gmail.com>
2026-09-11 01:27:44 +0100

    feat: Ajout de la structure du frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-500">Test Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
